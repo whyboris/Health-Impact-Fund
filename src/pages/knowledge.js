8e1409cc6550725a39fbffecb8f7629ca9b6ecfa
whyboris <yboris@yahoo.com>
2019-09-29 16:05:00 -0400

    better download link

--- src/pages/knowledge.js
+++ src/pages/knowledge.js
@@ -4,6 +4,8 @@ import { FormattedMessage, injectIntl } from "gatsby-plugin-intl"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import Icon from "../images/pdf.svg"
+
 const SecondPage = ({ intl }) => (
   <Layout>
     <SEO
@@ -60,121 +62,121 @@ const SecondPage = ({ intl }) => (
     <div class="publication_container">
       <ul>
         <li>
-          <strong>Aidan Hollis:</strong>
-          <span>“The Health Impact Fund and Price Determination”</span>
-          <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP1_Hollis.pdf">Download Link</a>
+          <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP1_Hollis.pdf">
+            <Icon />
+            <strong>Aidan Hollis</strong>
+            <span>The Health Impact Fund and Price Determination</span>
+          </a>
         </li>
 
         <li>
-          <strong>Talha Syed:</strong>
-          <span>
-            “Should a Prize System for Pharmaceuticals Require Patent Protection
-          </span>
-          <span>
-            <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP2_Syed.pdf">Download Link</a>
-          </span>
+          <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP2_Syed.pdf">
+            <Icon />
+            <strong>Talha Syed</strong>
+            <span>
+              Should a Prize System for Pharmaceuticals Require Patent
+              Protection
+            </span>
+          </a>
         </li>
 
         <li>
-          <strong>Thomas J. Bollyky:</strong>
-          <span>
-            “The Relationship of the Health Impact Fund and Its Registrants”
-          </span>
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP3_Bollyky.pdf">
-            Download Link
+            <Icon />
+            <strong>Thomas J. Bollyky</strong>
+            <span>
+              The Relationship of the Health Impact Fund and Its Registrants
+            </span>
           </a>
         </li>
 
         <li>
-          <strong>Kalipso Chalkidou:</strong>
-          <span>
-            “Assessing the Added Value of Health Technologies: NICE’s Experience
-          </span>
-
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP4_Chalkidou.pdf">
-            Download Link
+            <Icon />
+            <strong>Kalipso Chalkidou</strong>
+            <span>
+              Assessing the Added Value of Health Technologies: NICE’s
+              Experience
+            </span>
           </a>
         </li>
         <li>
-          <strong>Juggs Ravalia and Lennart Stern:</strong>
-          <span>
-            “The MHIF System: Supporting the Health Impact Fund with Mobile
-          </span>
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP5_Ravallia_and_Stern.pdf">
-            Download Link
+            <Icon />
+            <strong>Juggs Ravalia and Lennart Stern</strong>
+            <span>
+              The MHIF System: Supporting the Health Impact Fund with Mobile
+            </span>
           </a>
         </li>
 
         <li>
-          <strong>Rudolf V. Van Puymbroeck:</strong>
-          <span>“The Health Impact Fund: Creation and Commitment.”</span>
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP6_Van_Puymbroeck.pdf">
-            Download Link
+            <Icon />
+            <strong>Rudolf V. Van Puymbroeck</strong>
+            <span>The Health Impact Fund: Creation and Commitment.</span>
           </a>
         </li>
         <li>
-          <strong>Peter Singer and Doris Schroeder, lead authors:</strong>
-          <span>
-            “Ethical Reasons for Intellectual Property Rights Reform: A Report
-          </span>
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP7_Singer_and_Schroeder.pdf">
-            Download Link
+            <Icon />
+            <strong>Peter Singer and Doris Schroeder, lead authors</strong>
+            <span>
+              Ethical Reasons for Intellectual Property Rights Reform: A Report
+            </span>
           </a>
         </li>
         <li>
-          <strong>Joy Mendel and Aidan Hollis:</strong>
-          <span>“The Health Impact Fund and Traditional Medicines.”</span>
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP8_Mendel_and_Hollis.pdf">
-            Download Link
+            <Icon />
+            <strong>Joy Mendel and Aidan Hollis</strong>
+            <span>The Health Impact Fund and Traditional Medicines.</span>
           </a>
         </li>
         <li>
-          <strong>Aidan Hollis and Thomas Pogge:</strong>
-          <span>
-            “Product- Development Partnerships and the Health Impact Fund.”
-          </span>
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/DP9_Hollis-Pogge_PDPs.pdf">
-            Download Link
+            <Icon />
+            <strong>Aidan Hollis and Thomas Pogge</strong>
+            <span>
+              Product-Development Partnerships and the Health Impact Fund.
+            </span>
           </a>
         </li>
 
         <li>
-          <strong>Aidan Hollis and Thomas Pogge:</strong>
-          <span>
-            “ONU: groupe de haut niveau sur l&#8217;accès aux traitements
-            médicaux
-          </span>
-
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/High-level-panel-draft-2016-02-23-en-français.pdf">
-            Download Link
+            <Icon />
+            <strong>Aidan Hollis and Thomas Pogge</strong>
+            <span>
+              ONU: groupe de haut niveau sur l'accès aux traitements médicaux
+            </span>
           </a>
         </li>
         <li>
-          <strong>Ishaan Nerurkar &#038; Colton Jang:</strong>
-          <span>“The Health Impact Fund: A Mechanism Design Approach.”</span>
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/HIF_Mechanism_Discussion_Paper.pdf">
-            Download Link
+            <Icon />
+            <strong>Ishaan Nerurkar & Colton Jang</strong>
+            <span>The Health Impact Fund: A Mechanism Design Approach.</span>
           </a>
         </li>
 
         <li>
-          <strong>Thomas Pogge &#038; Aidan Hollis:</strong>
-          <span>
-            “Secretary-General&#8217;s High-Level Panel on Access to Medicines
-          </span>
-
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/High-level-panel-draft-016-02-22-formatted.docx.pdf">
-            Download Link
+            <Icon />
+            <strong>Thomas Pogge & Aidan Hollis</strong>
+            <span>
+              Secretary-General's High-Level Panel on Access to Medicines
+            </span>
           </a>
         </li>
 
         <li>
-          <strong>Thomas Pogge &#038; Aidan Hollis:</strong>
-          <span>
-            “Response to the High-Level Panel and Expert Advisory Group&#8221;
-          </span>
           <a href="https://healthimpactfund.org/wp-content/uploads/2015/12/High-level-panel-response-2016-03-16.docx.pdf">
-            Download Link
+            <Icon />
+            <strong>Thomas Pogge & Aidan Hollis</strong>
+            <span>
+              Response to the High-Level Panel and Expert Advisory Group
+            </span>
           </a>
         </li>
       </ul>
