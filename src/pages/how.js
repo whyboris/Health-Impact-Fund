d4746fa2586e6ddae1b942029fda037dccbd0586
whyboris <yboris@yahoo.com>
2019-11-04 19:21:54 -0500

    how same as about

--- src/pages/how.js
+++ src/pages/how.js
@@ -62,20 +62,20 @@ const Benefits = ({ intl }) => (
       product for annual reward payments.
     </p>
     <p>
-      <strong>One special feature:</strong>
+      <strong>Control of prices to support access:</strong>
       <br />
-      the price of registered products is limited to the costs of manufacture
+      The price of registered products is limited to the costs of manufacture
       and distribution, and therefore affordable even for poor patients. The
       price of registered medicines is delinked from their R&D cost.
     </p>
 
     <p>
-      <strong>A second special feature:</strong>
+      <strong>Competitive reward payments based on performance:</strong>
       <br />
-      reward payments for pharmaceutical innovators depend solely on the annual
+      Reward payments for pharmaceutical innovators depend solely on the annual
       health gains achieved by their registered medicines. The more such a new
-      medicine improves or lengthens human lives, the more money goes to its
-      innovator.
+      medicine improves or lengthens human lives, the more money the innovator
+      earns.
     </p>
 
     <h1 id="background">Background</h1>
@@ -87,28 +87,28 @@ const Benefits = ({ intl }) => (
       </strong>
       <br />
       The development of new medicines is very expensive. To cover these R&D
-      costs, states provide 20-year patents. Under the protection of such
-      temporary monopolies, pharmaceutical firms sell their new products at very
-      high prices. This system has two adverse effects:
+      costs, countries offer 20-year patents. Under the protection of these
+      temporary monopolies, pharmaceutical firms can sell their new products at
+      very high prices. This system has two adverse effects:
     </p>
     <p>
-      <strong>First adverse effect:</strong>
+      <strong>Little research into diseases of poor people:</strong>
       <br />
-      in the current system, the diseases of the poor are unattractive areas for
+      In the current system, the diseases of the poor are unattractive for
       pharmaceutical research. This is so because poor patients cannot afford to
       buy expensive medicines. The diseases of the poor are therefore generally
-      neglected by pharmaceutical research. Remedies against hair loss are more
+      neglected by pharmaceutical companies. Remedies against hair loss are more
       likely to be sought than remedies against deadly diseases of poverty, such
       as dengue, leishmaniasis or Ebola.
     </p>
     <p>
-      <strong>Second adverse effect:</strong>
+      <strong>High prices limit access to new drugs:</strong>
       <br />
-      new medicines are generally unaffordable for the poor. Even when new
-      medicines are developed, against hepatitis C for example, they are almost
-      always sold at profit-maximizing monopoly prices. These far exceed what
-      most patients can afford. The same also holds for medicines against global
-      diseases such as cancer.
+      New medicines are generally unaffordable for the poor. Even when new
+      medicines are developed, for example against the hepatitis C virus, they
+      are almost always sold at profit-maximizing monopoly prices. These far
+      exceed what most patients can afford. The same also holds for medicines
+      against global diseases such as cancer.
     </p>
     <p>
       <strong>
@@ -117,7 +117,7 @@ const Benefits = ({ intl }) => (
       </strong>
       <br />
       With the Health Impact Fund, pharmaceutical companies obtain an additional
-      option that, through new incentives, mitigates the two adverse effects.
+      option that, through new incentives, mitigates these two adverse effects.
     </p>
 
     <h1 id="essence">The Essence</h1>
@@ -125,28 +125,31 @@ const Benefits = ({ intl }) => (
       <strong>Health gains as the standard.</strong>
       <br />
       The purpose of medicines is to improve and preserve health. The Health
-      Impact Fund aligns research, development and marketing with precisely this
-      purpose. This is so because the reward payments that any innovator
-      receives for its registered innovation result exclusively from the health
-      gains that this medicine achieves year by year. The more a registered
-      product lengthens or improves human lives, the higher are the reward
-      payments that its innovator receives from the Health Impact Fund. In this
-      calculus, the health of all human beings is weighted equally, regardless
-      of whether they are rich or poor.
+      Impact Fund aligns corporate incentives to engage in research, development
+      and marketing with precisely this social objective. This is so because the
+      reward payments that any innovator receives for its registered innovation
+      are tied directly to the health gains that this medicine achieves year by
+      year. The more a registered product lengthens or improves human lives, the
+      higher are the reward payments that its innovator receives from the Health
+      Impact Fund. In this calculus, the health of all human beings is weighted
+      equally, regardless of whether they are rich or poor.
     </p>
     <p>
       <strong>Covering the costs of pharmaceutical firms.</strong>
       <br />
       With the publicly funded reward payments that the Health Impact Fund pays
-      out for each registered medicine, the firm can recoup its R&D costs and
-      earn profits in addition.
+      out for each registered medicine, the firm bears the risks of failure in
+      case its product has little health benefit and earns the rewards of
+      success, in case its product is therapeutically very successful. On
+      average, firms should be able to cover their costs and earn a competitive
+      rate of return.
     </p>
     <p>
       <strong>Delinking a medicine’s price from its R&D costs.</strong>
       <br />
-      Registered medicines can then be sold at the affordable cost price, which
-      covers merely the variable costs of manufacture and distribution. Such
-      medicines are therefore affordable even to very poor patients.
+      Registered medicines can be sold at an affordable price, which covers
+      merely the costs of manufacture and distribution. Such medicines are
+      therefore affordable even to very poor patients.
     </p>
   </Layout>
 )
