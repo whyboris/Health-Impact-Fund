3a8c2ff16c8cda2422a06b036581949ddff514bc
whyboris <yboris@yahoo.com>
2019-11-16 13:19:09 -0500

    what -> i18n

--- src/pages/what.js
+++ src/pages/what.js
@@ -1,8 +1,5 @@
 import React from "react"
-import {
-  // FormattedMessage,
-  injectIntl,
-} from "gatsby-plugin-intl"
+import { FormattedMessage, injectIntl } from "gatsby-plugin-intl"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -45,26 +42,24 @@ const Benefits = ({ intl }) => {
         </svg>
       </div>
 
-      <h1 id="summary">What is the HIF?</h1>
+      <h1 id="summary">
+        <FormattedMessage id="what.title" />
+      </h1>
 
       <p>
-        The Health Impact Fund is intended to provide a complementary system for
-        the development of pharmaceutical innovations – especially ones intended
-        for poor patients who cannot afford expensive medicines.
+        <FormattedMessage id="what.intro" />
       </p>
-      <strong>How does it work?</strong>
+      <strong>
+        <FormattedMessage id="what.how" />
+      </strong>
       <p>
-        The Health Impact Fund will be financed by states and charitable
-        foundations. It would give pharmaceutical innovators the option of
-        registering any new product for annual reward payments.
+        <FormattedMessage id="what.how_answer" />
       </p>
-      <strong>Prices</strong>
+      <strong>
+        <FormattedMessage id="what.prices" />
+      </strong>
       <p>
-        The price of any product registered with the HIF is limited to the costs
-        of manufacture and distribution, and therefore affordable even for poor
-        patients. The price of registered medicines is delinked from their R&D
-        cost. One way to keep prices down is to enable generic manufacturers to
-        compete in producing and selling registered drugs.
+        <FormattedMessage id="what.prices_answer" />{" "}
       </p>
 
       <strong
@@ -78,34 +73,30 @@ const Benefits = ({ intl }) => {
           showLongDescription1 = !showLongDescription1
         }}
       >
-        See more
+        <FormattedMessage id="what.see_more" />
       </strong>
 
       <p id="first" className="description-long hidden">
-        Usually, once drugs become generic, prices fall substantially since
-        generic manufacturers compete to sell identical products. Generic
-        competition may not work in all situations, so the HIF might use other
-        tools to ensure low prices, including tendering or regulated prices.
+        <FormattedMessage id="what.prices_see_more" />
       </p>
 
       <p style={{ "text-align": "center" }}>
-        For much more information on price determination, see{" "}
+        <FormattedMessage id="what.for_more_info_see" />{" "}
         <a
           className="see-more-click"
           style={{ display: "inline-block" }}
           href="/pdf/DP1_Hollis.pdf"
         >
-          document
+          <FormattedMessage id="what.document" />
         </a>
       </p>
 
       <p>
-        <strong>Reward payments</strong>
+        <strong>
+          <FormattedMessage id="what.reward" />
+        </strong>
         <br />
-        The reward payments received by pharmaceutical innovators depend solely
-        on the annual health gains achieved by their registered medicines. The
-        more such a new medicine improves or lengthens human lives, the more
-        money goes to its innovator.
+        <FormattedMessage id="what.reward_answer" />
       </p>
 
       <strong
@@ -119,20 +110,11 @@ const Benefits = ({ intl }) => {
           showLongDescription2 = !showLongDescription2
         }}
       >
-        See more
+        <FormattedMessage id="what.see_more" />
       </strong>
 
       <p id="second" className="description-long hidden">
-        An important part of the HIF model is that the HIF should pay out a
-        fixed amount in every year. Registered products obtain a share of the
-        fixed payout equal to their share of assessed health impact of all
-        registered products annually. Thus, firms are essentially competing to
-        improve human health. Having a fixed total payout divided between firms
-        helps the HIF (and funders) to manage its budget. Each registered
-        product would be eligible for ten years of payments, which resembles the
-        duration of exclusivity that firms now enjoy for patented
-        pharmaceuticals. Following the 10 years, products would be required to
-        be licensed for generic production.
+        <FormattedMessage id="what.reward_see_more" />
       </p>
     </Layout>
   )
