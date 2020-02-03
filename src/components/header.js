6f3ee2a88bc4aa4f6686dec8f8c3ab9f63edd612
whyboris <yboris@yahoo.com>
2020-02-03 10:40:53 -0500

    language import

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby-plugin-intl"
 import PropTypes from "prop-types"
 import React from "react"
-// import Language from "./language"
+import Language from "./language"
 
 import { injectIntl, FormattedMessage } from "gatsby-plugin-intl"
 
