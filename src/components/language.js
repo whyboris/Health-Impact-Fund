e5010e36837285ce70885d90ef6ffa610c79e3f6
whyboris <yboris@yahoo.com>
2020-02-09 15:31:23 -0500

    better language selection

--- src/components/language.js
+++ src/components/language.js
@@ -1,19 +1,34 @@
 import React from "react"
 import { IntlContextConsumer, changeLocale } from "gatsby-plugin-intl"
 
-const languageName = {
-  ar: "Arabic / عربى",
+const languageName1 = {
+  ar: "Arabic",
+  de: "German",
+  en: "English",
+  es: "Spanish",
+  fr: "French",
+  hi: "Hindi",
+  id: "ID",
+  it: "Italian",
+  ko: "Korean",
+  tr: "Turkish",
+  ur: "Urdu",
+  zh: "Chinese",
+}
+
+const languageName2 = {
+  ar: "عربى",
   de: "Deutsch",
   en: "English",
   es: "Español",
-  fr: "French / Français",
-  hi: "Hindi / हिंदी",
-  id: "ID / ID",
-  it: "Italian / Italiano",
-  ko: "Korean / 한국어",
-  tr: "Turkish / Türk",
-  ur: "Urdu / اردو",
-  zh: "Chinese / 中文",
+  fr: "Français",
+  hi: "हिंदी",
+  id: "ID",
+  it: "Italiano",
+  ko: "한국어",
+  tr: "Türk",
+  ur: " اردو",
+  zh: "中文",
 }
 
 const Language = () => {
@@ -30,7 +45,12 @@ const Language = () => {
                 color: currentLocale === language ? `#80CF0F` : `#222222`,
               }}
             >
-              {languageName[language]}
+              <div className="language-english">
+                {languageName1[language]}
+              </div>
+              <div className="language-original">
+                {languageName2[language]}
+              </div>
             </span>
           ))
         }
