1dab2be3fd4524072ed80532d61e73a78d798674
whyboris <yboris@yahoo.com>
2020-12-13 18:37:43 -0500

    large image when sharing on twitter

--- src/components/seo.js
+++ src/components/seo.js
@@ -53,7 +53,7 @@ function SEO({ description, lang, meta, keywords, title }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:image`,
